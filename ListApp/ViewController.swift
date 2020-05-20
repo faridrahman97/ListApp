ed4fdc53b2228aa6717fbe365ce9151f5beb025a
faridrahman97 <beatsbyfar.id@gmail.com>
2020-05-19 21:47:13 -0700

    Initial Commit

---
+++ ListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListApp
+//
+//  Created by Farid Rahman on 5/19/20.
+//  Copyright © 2020 Farid Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
